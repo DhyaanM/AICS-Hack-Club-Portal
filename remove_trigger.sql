e5d813a1d1cf20cc7ae6cc85b76b4443626f420a
Dhyaan Manganahalli <s936832@aics.espritscholen.nl>
2026-03-04 19:39:27 +0100

    chore: cleanup temporary sql scripts

--- remove_trigger.sql
+++
@@ -1,5 +0,0 @@
--- Remove the auto-creation trigger
-DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
-
--- Remove the function that automatically adds users
-DROP FUNCTION IF EXISTS public.handle_new_user();
